4e0d2b0b15e9d6c2a23afbcd35a52df146c06b1e
CodeRacon <michael.buschmann.86@gmail.com>
2025-03-05 19:33:28 +0100

    Initial commit: Angular 18 project setup with Firebase integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"roomfull-crm","appId":"1:58051527929:web:f7ed20be8314c1e22a4e28","storageBucket":"roomfull-crm.firebasestorage.app","apiKey":"AIzaSyBzoLtZSHNkEExAjPQQ_ck6a1KFSYbVE3o","authDomain":"roomfull-crm.firebaseapp.com","messagingSenderId":"58051527929"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideAnimationsAsync()]
 };
