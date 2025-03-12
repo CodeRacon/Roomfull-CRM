ebcd24ee6970b7b0b14febcf52ebef56169b5d06
CodeRacon <michael.buschmann.86@gmail.com>
2025-03-12 20:41:18 +0100

    feat(rooms): implement rooms module with list, details and booking components

--- src/app/features/rooms/rooms-routing.module.ts
+++ src/app/features/rooms/rooms-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/room-list/room-list.component').then(
+        (m) => m.RoomListComponent
+      ),
+  },
+  {
+    path: ':id',
+    loadComponent: () =>
+      import('./components/room-details/room-details.component').then(
+        (m) => m.RoomDetailsComponent
+      ),
+  },
+  {
+    path: 'book/:id',
+    loadComponent: () =>
+      import('./components/room-booking/room-booking.component').then(
+        (m) => m.RoomBookingComponent
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class RoomsRoutingModule { }
+export class RoomsRoutingModule {}
