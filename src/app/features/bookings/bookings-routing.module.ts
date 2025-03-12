81f78c2021e1791fe5f3239e2793551c33f891a1
CodeRacon <michael.buschmann.86@gmail.com>
2025-03-12 20:41:28 +0100

    feat(bookings): create booking module with list and details views

--- src/app/features/bookings/bookings-routing.module.ts
+++ src/app/features/bookings/bookings-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/booking-list/booking-list.component').then(
+        (m) => m.BookingListComponent
+      ),
+  },
+  {
+    path: ':id',
+    loadComponent: () =>
+      import('./components/booking-details/booking-details.component').then(
+        (m) => m.BookingDetailsComponent
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class BookingsRoutingModule { }
+export class BookingsRoutingModule {}
