5b2959b601350567f2c0b0ea2e74d0a115d18aab
CodeRacon <michael.buschmann.86@gmail.com>
2025-03-12 23:29:53 +0100

    chore(config): update app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,5 +9,25 @@ import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"roomfull-crm","appId":"1:58051527929:web:f7ed20be8314c1e22a4e28","storageBucket":"roomfull-crm.firebasestorage.app","apiKey":"AIzaSyBzoLtZSHNkEExAjPQQ_ck6a1KFSYbVE3o","authDomain":"roomfull-crm.firebaseapp.com","messagingSenderId":"58051527929"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyBzoLtZSHNkEExAjPQQ_ck6a1KFSYbVE3o',
+        authDomain: 'roomfull-crm.firebaseapp.com',
+        projectId: 'roomfull-crm',
+        storageBucket: 'roomfull-crm.firebasestorage.app',
+        messagingSenderId: '58051527929',
+        appId: '1:58051527929:web:f7ed20be8314c1e22a4e28',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideAnimationsAsync(),
+    provideAnimationsAsync(),
+    provideAnimationsAsync(),
+    provideAnimationsAsync(),
+  ],
 };
