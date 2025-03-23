dd2cc5bc417dfff3326a6795463043651c5ddce5
CodeRacon <michael.buschmann.86@gmail.com>
2025-03-23 19:52:04 +0100

    refactor: update AppComponent structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { RoomService } from './core/services/room.service';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,11 @@ import { FooterComponent } from './shared/components/footer/footer.component';
 })
 export class AppComponent {
   title = 'roomfull';
+
+  constructor(private roomService: RoomService) {}
+
+  ngOnInit(): void {
+    // Seed initial rooms if none exist
+    this.roomService.seedInitialRooms().subscribe();
+  }
 }
